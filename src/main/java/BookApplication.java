35b0fcd13e686dd5c3c86b5a65713e26f874a1b2
Anna W <tester.kodilla@gmail.com>
2021-04-18 22:10:30 +0100

    removing files added in the wrong place

--- src/main/java/BookApplication.java
+++
@@ -1,9 +0,0 @@
-public class BookApplication {
-    public static void main(String[] args) {
-        Book book = Book.of("Isaac Asimov", "The Galaxy");
-        System.out.println(book.getAuthor() + " " + book.getTitle());
-
-        Book book2 = Book.of("Ken Follett", "World without end");
-        System.out.println(book2.getAuthor() + " " + book2.getTitle());
-    }
-}
